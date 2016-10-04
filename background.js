1f64fa0260e9b16dab7e2f050dddfb6643937b9b
emmanuelsf <emmanuel.sf@hotmail.com>
2016-10-03 22:58:03 -0300

    Refactoring, avoid email, change icon, read links, get timestamps, get id

--- background.js
+++ background.js
@@ -42,41 +42,48 @@ function executeMailto(tab_id, subject, body, selection) {
   }
 }
 
+//Listen to the response of the content script
 chrome.runtime.onConnect.addListener(function(port) {
   var tab = port.sender.tab;
 
   // This will get called by the content script we execute in
   // the tab as a result of the user pressing the browser action.
   port.onMessage.addListener(function(info) {
+    
     var max_length = 1024;
     var posts_text="POSTS: ";
 
     if (info.selection.length > max_length)
       info.selection = info.selection.substring(0, max_length);
-    if(info.posts.length>0){
-      info.post_quantity="Posts: "+info.posts.length;
-      for (var i = info.posts.length - 1; i >= 0; i--) {
-        posts_text+=info.posts[i];
-        posts_text+=" - ";
-      }
+    if(info.basic_posts.length>0){
+      info.post_quantity="Posts econtrados: "+info.basic_posts.length;
     }
     else{
       info.post_quantity="Ningún post";
     }
-
-
-    executeMailto(tab.id, info.title, tab.url, posts_text);
+    console.log(info.post_quantity);
+    //executeMailto(tab.id, info.title, tab.url, posts_text);
   });
 });
 
+// If it is possible inject the content script if not send the email
 // Called when the user clicks on the browser action icon.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(tab) {  
   // We can only inject scripts to find the title on pages loaded with http
   // and https so for all other pages, we don't ask for the title.
-  if (tab.url.indexOf("http:") != 0 &&
-      tab.url.indexOf("https:") != 0) {
-    executeMailto(tab.id, "", tab.url, "");
-  } else {
+  if (tab.url.indexOf("https://www.facebook.com/groups/") != -1) {
+    //executeMailto(tab.id, "", tab.url, "");
+    console.log("injecting content_script ");
     chrome.tabs.executeScript(null, {file: "content_script.js"});
-  }
+  }  
 });
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+    if(tab.url.indexOf("https://www.facebook.com/groups/") == -1){
+      console.log("disabling extension");
+      chrome.browserAction.disable(tab.id);
+      //chrome.browserAction.setIcon({path: "note-grey.png"});
+    }
+  }
+})
--- content_script.js
+++ content_script.js
@@ -2,32 +2,88 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//Get group
+var group = 'https://www.facebook.com'+$('#fbProfileCover > div._5mo5._5t1f > div > div:nth-child(2) > h1 > a').attr('href');
 
 var posts = [];
 
-function post(_description, _user, _url, _timestamp, _datetime) {
-    this.description = _description;
-    this.user = _user;
-    this.url = _url;
-    this.timestamp = _timestamp;
-    this.datetime = _datetime;
+function Post() {
+    this.description;
+    this.user;
+    this.profile;
+    this.utime;    
+    this.link;
+    this.photo; //Research hoy to upload and get link
+	this.date;
+	this.time;
+	this.id;
+	this.url;
+	this.group;
+
+    this.set_date_and_time = function() {
+    	if (this.utime != undefined && this.utime != null) {
+    		
+			// Create a new JavaScript Date object based on the timestamp
+			// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+			this.date = new Date(this.utime*1000);
+			// Hours part from the timestamp
+			var hours = this.date.getHours();
+			// Minutes part from the timestamp
+			var minutes = "0" + this.date.getMinutes();
+			// Seconds part from the timestamp
+			var seconds = "0" + this.date.getSeconds();
+			// Will display time in 10:30:23 format
+			this.time = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);    		
+    	}
+    }
+
+    this.set_url = function() {
+    	this.url = group+'permalink/'+this.id;
+    }
 }
 
+//Prepare DOM
 $('span.see_more_link_inner').remove();
-$('div._1dwg._1w_m').each(
-	function(){
-		var new_post = new post();
-		new_post.description=$(this).find('div._5pbx.userContent p').text();
-		new_post.description=new_post.description.replace('...','.');
-		new_post.description=new_post.description.replace('..','');
-		
-		new_post.user=$(this).find('h5._5pbw._5vra span span a').text();
-		new_post.url=$(this).find('h5._5pbw._5vra span span a').attr('href');
-		
-		new_post.datetime = $(this).find('abbr._5ptz.timestamp.livetimestamp').attr('title');
-		new_post.timestamp = $(this).find('abbr._5ptz.timestamp.livetimestamp').attr('data-utime');
 
-		posts.push(new_post);
+
+//Read POSTS
+$('div._4-u2.mbm._5jmm._5pat._5v3q._4-u8').each(
+	function(){
+		
+		var new_post = new Post();
+		
+		new_post.group = group;
+
+		new_post.description = $(this).find('div._5pbx.userContent p').text();
+		new_post.description = new_post.description.replace('...','.');
+		new_post.description = new_post.description.replace('..','');
+		
+		new_post.id = $(this).attr('id').substr(10).slice(0,-4);
+
+		//Search for user
+		new_post.user = $(this).find('h5._5pbw._5vra span.fwn.fcg span.fwb.fcg a').text();
+
+		//Search for user profile
+		new_post.profile = $(this).find('h5._5pbw._5vra span.fwn.fcg span.fwb.fcg a').attr('href');
+		
+		new_post.photo = $(this).find('div._3x-2 div div.mtm div._5cq3 a').attr('href');
+
+		//Search for link
+		new_post.link = $(this).find('div._3x-2 div._6m3._--6 a').attr('href');
+		if (new_post.link == undefined) {
+			new_post.link = $(this).find('div._3x-2 div._3ekx._29_4 a').attr('href');			
+		}
+		
+		//Search for utime
+		new_post.utime  =  $(this).find('abbr._5ptz.timestamp.livetimestamp').attr('data-utime');
+		if (new_post.utime == undefined) {
+			new_post.utime  =  $(this).find('abbr._5ptz').attr('data-utime');			
+		}
+		new_post.set_date_and_time();
+		new_post.set_url();
+
+		posts.push(new_post);		
+		
 	}
 );    
 
